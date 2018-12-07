ef17aa6dcde42fc3fc075f91e064882884ba3e4a
周明 <hzzhouming@corp.netease.com>
2018-12-07 14:23:55 +0800

    init: init project

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+  return sass('scss/styles.scss')
+    .pipe(gulp.dest('app/css'))
+    .pipe(reload({ stream:true }));
+});
+
+// 监视 Sass 文件的改动，如果发生变更，运行 'sass' 任务，并且重载文件
+gulp.task('serve', ['sass'], function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch('app/scss/*.scss', ['sass']);
+});
